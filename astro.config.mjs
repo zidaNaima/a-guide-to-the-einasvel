46b2341e478e381a02a01b324289cd44fb93ddbf
Naima <naima.zida8@gmail.com>
2026-06-27 18:06:32 -0700

    build: tailwind switched to astro intergration. react integration added. cleanup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-        server: {
-          watch: {
-            usePolling: true,
-          }
+  integrations: [react(), tailwind()],
+  vite: {
+      server: {
+        watch: {
+          usePolling: true,
         }
-    }
+      }
+  }
 });
