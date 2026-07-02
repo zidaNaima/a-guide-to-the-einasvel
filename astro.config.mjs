f5739fede35a834d91087a3a8ca684264172dcfa
Naima <naima.zida8@gmail.com>
2026-07-01 19:42:12 -0700

    build: Switch to TailwindCSS addon due to new Astro version.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   vite: {
       server: {
         watch: {
